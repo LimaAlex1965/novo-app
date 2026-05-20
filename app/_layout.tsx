683c666ec6f3ea86c9fae73acb841329c09e36d6
LimaAlex1965 <alvesdelima.alex@gmail.com>
2026-05-20 00:05:53 +0000

    mudamos o index e layout e criamo o sobre

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
-import { Stack } from "expo-router";
+import { Drawer } from "expo-router/drawer";
+import { View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Drawer 
+      screenOptions={{
+        drawerActiveTintColor: '#fff',  
+        drawerActiveBackgroundColor: '#b98952',
+        drawerInactiveTintColor: '#f30f0f',
+      }}>
+        
+       <Drawer.Screen options={{title:"Pricipal", headerTitle: "kkkkk"}}  name="index"/>
+    </Drawer>;
 }
